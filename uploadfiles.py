f09f040a14465ec78296f4ba021cd9ad40ea2f62
Yûki Vachot <yuki@vachot.fr>
2021-10-13 11:04:47 +0200

    Add get_filenames

--- uploadfiles.py
+++ uploadfiles.py
@@ -4,9 +4,9 @@ https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3.html?highlight=
 http://ls.pwd.io/2013/06/parallel-s3-uploads-using-boto-and-threads-in-python/
 """
 import sys
-from os import listdir
-from boto.s3.connection import S3Connection
-import threading
+import os
+#from boto.s3.connection import S3Connection
+#import threading
 
 
 # args needed to launch the program 
@@ -15,11 +15,21 @@ import threading
 #   folder_path
 
 def get_filenames(folder_path):
-  return listdir(folder_path)
+  filenames = []
+  for path, subdirs, files in os.walk(folder_path):
+    for name in files:
+        filenames.append(os.path.join(path, name))
+  return filenames
 
-def main():
-  get_filenames("")
+def main(AWS_KEY, AWS_SECRET_KEY, FOLDER_PATH):
+  print(os.getcwd())
+  filenames = get_filenames(FOLDER_PATH)
+  print(filenames)
 
 
 if __name__ == "__main__":
-  main()
+  # TODO	Get Sys Args for variable
+  AWS_KEY = ""
+  AWS_SECRET_KEY = ""
+  FOLDER_PATH = "test/"
+  main(AWS_KEY, AWS_SECRET_KEY, FOLDER_PATH)
